5c730db2ca78ab3ff3556b38dd8a587f8968ea0a
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-20 07:45:22 +0100

    small semantic tweak

--- ExpensesTrackerApp/src/components/Expenses/Expenses.js
+++ ExpensesTrackerApp/src/components/Expenses/Expenses.js
@@ -1,25 +1,25 @@
 import "./Expenses.css";
 import Card from "../UI/Card.js";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpensesList from "./ExpensesList.js"
+import ExpensesList from "./ExpensesList.js";
 import React, { useState } from "react";
 
-
 function Expenses(props) {
-  const [pickedYear, setPickedYear] = useState("2021")
+  const [pickedYear, setPickedYear] = useState("2021");
   const onPickedYearHandler = (pickedYear) => {
     setPickedYear(pickedYear);
   };
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={pickedYear}
-        onPickedYear={onPickedYearHandler}
-      />
-      <ExpensesList pickedYear={pickedYear} expenses={props.expenses} />
-
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={pickedYear}
+          onPickedYear={onPickedYearHandler}
+        />
+        <ExpensesList pickedYear={pickedYear} expenses={props.expenses} />
+      </Card>
+    </li>
   );
 }
 export default Expenses;
--- ExpensesTrackerApp/src/components/Expenses/ExpensesList.js
+++ ExpensesTrackerApp/src/components/Expenses/ExpensesList.js
@@ -2,24 +2,26 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem.js";
 
 const ExpensesList = (props) => {
-
   const expensesOfTheYear = props.expenses.filter(
-    (expense) => expense.date.getFullYear().toString() === props.pickedYear.toString()
+    (expense) =>
+      expense.date.getFullYear().toString() === props.pickedYear.toString()
   );
 
-
-console.log(props.pickedYear, expensesOfTheYear)
+  console.log(props.pickedYear, expensesOfTheYear);
   if (props.expenses.length === 0) {
     return <h2 className="expenses-list__fallback">No expenses found.</h2>;
   }
-  return expensesOfTheYear.map((expense) => (
+  return;
+  <ul className="expenses-list">
+    expensesOfTheYear.map((expense) => (
     <ExpenseItem
       key={expense.id}
       title={expense.title}
       amount={expense.amount}
       date={expense.date}
     />
-  ));
+    ));
+  </ul>;
 };
 
 export default ExpensesList;
