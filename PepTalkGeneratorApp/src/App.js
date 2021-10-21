0ebeb4dc72dbe4cabfc2361655b27ca554e474c3
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-10-21 11:25:22 +0100

    set up the project

---
+++ PepTalkGeneratorApp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Need pep talk?
+      </header>
+      <Card>
+
+      </Card>
+    </div>
+  );
+}
+
+export default App;
