abdaed7c8bf7dba25878ee094ad1be2855438674
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-10-22 16:59:25 +0100

    add another generator

--- PepTalkGeneratorApp/src/App.js
+++ PepTalkGeneratorApp/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import Card from "./Components/UI/Card.js"
-import RandomPepTalkGenerator from "./Components/RandomPepTalkGenerator.js"
+import "./App.css";
+import Card from "./Components/UI/Card.js";
+import RandomPepTalkGenerator from "./Components/RandomPepTalkGenerator.js";
+import RandomShakespeareInsultGenerator from "./Components/RandomShakespeareInsultGenerator.js";
 
 function App() {
   return (
     <div className="App">
-      
       <Card>
-      <header className="App-header">
-        Need pep talk?
-<RandomPepTalkGenerator />
-      </header>
+        <div className="App-header">
+          <RandomPepTalkGenerator />
+          <RandomShakespeareInsultGenerator />
+        </div>
+        {/* <div className="App-header">
+          <RandomShakespeareInsultGenerator />
+        </div> */}
       </Card>
     </div>
   );
