f9f6d301b15612e3b3a28a7ee93c0ea809d97bd7
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-23 09:09:10 +0100

    start implementing the logic

--- practice-project/src/Components/UserLogger.js
+++ practice-project/src/Components/UserLogger.js
@@ -1,15 +1,46 @@
 import "./UserLogger.css";
+import React, { useState } from "react";
 
 const UserLogger = (props) => {
+  const userData = {};
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const onUsernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const onAgeChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = (enteredUsername, enteredAge) => {
+    userData.username = enteredUsername;
+    userData.age = enteredAge;
+    console.log(userData);
+  };
+
   return (
     <form>
       <div>
         <h2 className="user-input__label">Username</h2>
-        <input type="text" className="user-input__field"></input>
-        <h2 className="user-input__label">Age</h2>
-        <input type="number" className="user-input__field"></input>
+        <input
+          type="text"
+          className="user-input__field"
+          onChange={onUsernameChangeHandler}
+        ></input>
+        <h2 className="user-input__label">Age (years)</h2>
+        <input
+          type="number"
+          className="user-input__field"
+          onChange={onAgeChangeHandler}
+        ></input>
       </div>
-      <button type="submit" className="user-input__submit">
+      <button
+        type="submit"
+        className="user-input__submit"
+        onSubmit={submitHandler}
+      >
         Add new user
       </button>
     </form>
