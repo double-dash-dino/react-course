27a65908dcba10e6ba6337539ad5aaff8f3f22fa
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-23 08:27:42 +0100

    more styles

--- practice-project/src/Components/UserLogger.js
+++ practice-project/src/Components/UserLogger.js
@@ -2,12 +2,17 @@ import "./UserLogger.css";
 
 const UserLogger = (props) => {
   return (
-    <div>
-      <h2 className="user-input__label">Username</h2>
-      <input type="text"></input>
-      <h2 className="user-input__label">Age</h2>
-      <input type="number"></input>
-    </div>
+    <form>
+      <div>
+        <h2 className="user-input__label">Username</h2>
+        <input type="text" className="user-input__field"></input>
+        <h2 className="user-input__label">Age</h2>
+        <input type="number" className="user-input__field"></input>
+      </div>
+      <button type="submit" className="user-input__submit">
+        Add new user
+      </button>
+    </form>
   );
 };
 
