e2450b5f6404def6573b5f77b1ddda30b21afc05
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-23 08:34:37 +0100

    styling still

--- practice-project/src/App.js
+++ practice-project/src/App.js
@@ -1,5 +1,6 @@
 import Card from "./Components/UI/Card.js";
 import UserLogger from "./Components/UserLogger.js";
+import UserList from "./Components/UserList.js";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="App">
       <Card>
         <UserLogger />
+      </Card>
+      <Card>
+        <UserList />
       </Card>
     </div>
   );
---
+++ practice-project/src/Components/UserList.js
@@ -0,0 +1,11 @@
+import "./UserList.css";
+
+const UserList = (props) => {
+  return (
+    <div>
+      <h2>User list</h2>
+    </div>
+  );
+};
+
+export default UserList;
