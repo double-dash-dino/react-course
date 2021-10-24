fe0d25d6140c7928ed043a4f450e9e8ab19e5d18
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-24 18:16:43 +0100

    can't use hooks at the app level so creating a component

--- RandomSentenceGeneratorsApp/src/App.js
+++ RandomSentenceGeneratorsApp/src/App.js
@@ -3,6 +3,7 @@ import Card from "./Components/UI/Card.js";
 import RandomPepTalkGenerator from "./Components/RandomPepTalkGenerator.js";
 import RandomShakespeareInsultGenerator from "./Components/RandomShakespeareInsultGenerator.js";
 import React, { useState } from "react";
+import RandomSentenceGenerator from "./Components/RandomSentenceGenerator.js";
 
 const [sentence, setSentence] = useState("");
 
@@ -15,9 +16,11 @@ function App() {
     <div className="App">
       <Card>
         <div className="App-header">
-          <RandomPepTalkGenerator getSentence={onGetSentence} />
-          <RandomShakespeareInsultGenerator getSentence={onGetSentence} />
-          <RandomSentence sentence={sentence} />
+          <RandomSentenceGenerator>
+            <RandomPepTalkGenerator getSentence={onGetSentence} />
+            <RandomShakespeareInsultGenerator getSentence={onGetSentence} />
+            <RandomSentence sentence={sentence} />
+          </RandomSentenceGenerator>
         </div>
       </Card>
     </div>
---
+++ RandomSentenceGeneratorsApp/src/RandomSentenceGenerator.js
@@ -0,0 +1,3 @@
+import "./RandomSentenceGenerator.css";
+
+const RandomSentenceGenerator = (props) => {};
