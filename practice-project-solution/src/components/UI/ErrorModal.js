bdd9b8de739f45ab4e4f0d43e68c19ab5b05a617
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-10-26 18:51:50 +0100

    add missing return statement

--- practice-project-solution/src/components/UI/ErrorModal.js
+++ practice-project-solution/src/components/UI/ErrorModal.js
@@ -10,17 +10,19 @@ const Backdrop = (props) => {
 };
 
 const ModalOverlay = (props) => {
-  <Card className={classes.modal}>
-    <header className={classes.header}>
-      <h2>{props.title}</h2>
-    </header>
-    <div className={classes.content}>
-      <p>{props.message}</p>
-    </div>
-    <footer className={classes.actions}>
-      <Button onClick={props.onConfirm}>Okay</Button>
-    </footer>
-  </Card>;
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
 };
 
 const ErrorModal = (props) => {
