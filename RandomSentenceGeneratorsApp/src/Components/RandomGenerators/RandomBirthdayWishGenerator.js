2535781adae4f23e7b8e0d51be9f4d9f005c2e69
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-10-26 09:17:37 +0100

    create components and add data

---
+++ RandomSentenceGeneratorsApp/src/Components/RandomGenerators/RandomBirthdayWishGenerator.js
@@ -0,0 +1,59 @@
+import "./RandomBirthdayWishGenerator.css";
+
+const RandomBirthdayWishGenerator = (props) => {
+  const sentenceSnippets = {
+    part1: [
+      "totally",
+      "seriously",
+      "legitimately",
+      "pretty much",
+      "like,",
+      "flippin'",
+      "really, really,",
+      "wholeheartedly",
+      "fully",
+      "gently & firmly",
+      "flat out",
+      "thoroughly",
+      "publicly",
+      "secretly",
+      "most definitely",
+    ],
+
+    part2: [
+      "rocks the Casbah",
+      "rains pure magic",
+      "turns water into wine",
+      "confirms your genius",
+      "is the bee's knees",
+      "is visible from space",
+      "makes kittens purr",
+      "deserves slow claps",
+      "works up a sweat",
+      "has all the feels",
+      "reaches for the stars",
+      "lets its hair down",
+      "is a dance marathon",
+      "sweeps the Nation",
+      "pumps up the volume",
+    ],
+
+    part3: [
+      "big time",
+      "4-ever",
+      "right?",
+      "end of story",
+      "you feel me?",
+      "'til dawn",
+      "with sparkles",
+      "period",
+      "based on science",
+      "like no tomorrow",
+      "to the max",
+      "no apologies",
+      "for 20 minutes",
+      "like a boss",
+      "because it's ON",
+    ],
+  };
+};
