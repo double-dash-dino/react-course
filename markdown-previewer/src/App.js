3ad249ecd9235b300a8065f87e710b75411c1455
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-11-07 20:11:55 +0000

    add some styling

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,20 +1,27 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("# Welcome to the markdown previewer");
+
+  const inputChangeHandler = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="input card">
         <div className="input-header">Editor</div>
         <textarea
+          onChange={inputChangeHandler}
           className="input-field"
           type="text"
-          value="
-          # Welcome to the markdown previewer"
+          value={input}
         ></textarea>
       </div>
       <div className="output card">
         <div className="output-header">Previewer</div>
-        <div className="output-ouput">There will be markdown here</div>
+        <div className="output-ouput">{input}</div>
       </div>
     </div>
   );
