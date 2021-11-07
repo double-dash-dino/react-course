d85ef8e7eee129068ab4bd350e90e85b3335925d
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-11-07 19:15:33 +0000

    start setting up components

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="input card">
+        <div className="input-header">Editor</div>
+        <textarea
+          className="input-field"
+          type="text"
+          value="
+          # Welcome to the markdown previewer"
+        ></textarea>
+      </div>
+      <div className="output card">
+        <div className="output-header">Previewer</div>
+        <div className="output-ouput">There will be markdown here</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
