48c8747d89ccf1c828b6af9173368136e54b8d08
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-11-08 09:02:30 +0000

    Add regex patterns for h tags

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -8,6 +8,31 @@ function App() {
     setInput(event.target.value);
   };
 
+  const returnMarkdownText = (text) => {
+    text = text.trim();
+    let h1Tag = /^#(?!#)/g;
+    let h2Tag = /^#{2}(?!#)/g;
+    let h3Tag = /^#{3}(?!#)/g;
+    let h4Tag = /^#{4}(?!#)/g;
+    let h5Tag = /^#{5}(?!#)/g;
+    let h6Tag = /^#{6}/g;
+    if (h1Tag.test(text)) {
+      text = <h1>{text.trim().replace(h1Tag, "")}</h1>;
+    } else if (h2Tag.test(text)) {
+      text = <h2>{text.trim().replace(h2Tag, "")}</h2>;
+    } else if (h3Tag.test(text)) {
+      text = <h3>{text.trim().replace(h3Tag, "")}</h3>;
+    } else if (h4Tag.test(text)) {
+      text = <h4>{text.trim().replace(h4Tag, "")}</h4>;
+    } else if (h5Tag.test(text)) {
+      text = <h5>{text.trim().replace(h5Tag, "")}</h5>;
+    } else if (h6Tag.test(text)) {
+      text = <h6>{text.trim().replace(h6Tag, "")}</h6>;
+    }
+
+    return text;
+  };
+
   return (
     <div className="App">
       <div className="input card">
@@ -21,7 +46,7 @@ function App() {
       </div>
       <div className="output card">
         <div className="output-header">Previewer</div>
-        <div className="output-ouput">{input}</div>
+        <div className="output-ouput">{returnMarkdownText(input)}</div>
       </div>
     </div>
   );
