a557527ac2c7b8c782b82928c63b752cdefd53ad
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-11-08 09:16:37 +0000

    get the h tags to work!

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -10,24 +10,27 @@ function App() {
 
   const returnMarkdownText = (text) => {
     text = text.trim();
+    text = text.split("\n");
     let h1Tag = /^#(?!#)/g;
     let h2Tag = /^#{2}(?!#)/g;
     let h3Tag = /^#{3}(?!#)/g;
     let h4Tag = /^#{4}(?!#)/g;
     let h5Tag = /^#{5}(?!#)/g;
     let h6Tag = /^#{6}/g;
-    if (h1Tag.test(text)) {
-      text = <h1>{text.trim().replace(h1Tag, "")}</h1>;
-    } else if (h2Tag.test(text)) {
-      text = <h2>{text.trim().replace(h2Tag, "")}</h2>;
-    } else if (h3Tag.test(text)) {
-      text = <h3>{text.trim().replace(h3Tag, "")}</h3>;
-    } else if (h4Tag.test(text)) {
-      text = <h4>{text.trim().replace(h4Tag, "")}</h4>;
-    } else if (h5Tag.test(text)) {
-      text = <h5>{text.trim().replace(h5Tag, "")}</h5>;
-    } else if (h6Tag.test(text)) {
-      text = <h6>{text.trim().replace(h6Tag, "")}</h6>;
+    for (let i = 0; i < text.length; i++) {
+      if (h1Tag.test(text[i])) {
+        text[i] = <h1>{text[i].trim().replace(h1Tag, "")}</h1>;
+      } else if (h2Tag.test(text[i])) {
+        text[i] = <h2>{text[i].trim().replace(h2Tag, "")}</h2>;
+      } else if (h3Tag.test(text[i])) {
+        text[i] = <h3>{text[i].trim().replace(h3Tag, "")}</h3>;
+      } else if (h4Tag.test(text[i])) {
+        text[i] = <h4>{text[i].trim().replace(h4Tag, "")}</h4>;
+      } else if (h5Tag.test(text[i])) {
+        text[i] = <h5>{text[i].trim().replace(h5Tag, "")}</h5>;
+      } else if (h6Tag.test(text[i])) {
+        text[i] = <h6>{text[i].trim().replace(h6Tag, "")}</h6>;
+      }
     }
 
     return text;
