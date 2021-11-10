36676f4b51b22ae1a657196685aaf6a876a2c36b
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-11-10 13:12:10 +0000

    start working on the expand button

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,25 +1,44 @@
 import "./App.css";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExpandArrowsAlt } from "@fortawesome/free-solid-svg-icons";
+import { faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 import { parse } from "marked/src/marked";
 
 // /* global marked */
 
 function App() {
   const [input, setInput] = useState("# Welcome to the markdown previewer");
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [display, setDisplay] = useState("");
+
+  const changeHandler = (props) => {};
 
   const inputChangeHandler = (event) => {
     setInput(event.target.value);
   };
 
+  let card_styles = "";
+
+  const clickHandler = (event) => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      card_styles = "";
+    } else {
+      setIsExpanded(true);
+      card_styles = " expanded";
+    }
+    console.log("is now", isExpanded, card_styles);
+  };
+
   return (
     <div className="App">
-      <div className="input card">
+      <div className={"input card" + card_styles} onChange={changeHandler}>
         <div className="card-header">
-          <div className="header-title">Editor</div>
-          <button className="expand-button">
-            <FontAwesomeIcon icon={faExpandArrowsAlt} />
+          <div className="header-title">
+            <h3>Editor</h3>
+          </div>
+          <button className="expand-button" onClick={clickHandler}>
+            <FontAwesomeIcon icon={faExpandAlt} className="fa-2x" />
           </button>
         </div>
         <textarea
@@ -31,9 +50,11 @@ function App() {
       </div>
       <div className="output card">
         <div className="card-header">
-          <div className="header-title">Editor</div>
+          <div className="header-title">
+            <h3>Output</h3>
+          </div>
           <button className="expand-button">
-            <FontAwesomeIcon icon={faExpandArrowsAlt} />
+            <FontAwesomeIcon icon={faExpandAlt} className="fa-2x" />
           </button>
         </div>
         <div
