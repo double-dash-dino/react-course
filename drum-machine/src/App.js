5085d6f653b405478168d49e90bde4d76563796d
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2021-11-11 13:39:35 +0000

    start adding listeners

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,23 +1,40 @@
 import "./App.css";
-
+const logKey = (event) => {
+  console.log(event.key === "q");
+  if (event.key === "q") {
+    document.getElementById("button1").click();
+  }
+};
+document.addEventListener("keydown", logKey);
 function App() {
   return (
     <div className="App">
       <div className="drum-card">
-        <div className="class-header">
-          Welcome to the app
+        <div className="card-header">Welcome to the app</div>
+        <div className="card-body">
           <div className="drum-pad">
-            <button className="drum-button">Q</button>
-            <button className="drum-button">W</button>
-            <button className="drum-button">E</button>
-            <button className="drum-button">A</button>
-            <button className="drum-button">S</button>
-            <button className="drum-button">D</button>
-            <button className="drum-button">Z</button>
-            <button className="drum-button">X</button>
-            <button className="drum-button">C</button>
+            <div>Pad header</div>
+            <div className="drum-buttons">
+              <button className="drum-button" value="q" id="button1">
+                Q
+              </button>
+              <button className="drum-button" value="w">
+                W
+              </button>
+              <button className="drum-button">E</button>
+              <button className="drum-button">A</button>
+              <button className="drum-button">S</button>
+              <button className="drum-button">D</button>
+              <button className="drum-button">Z</button>
+              <button className="drum-button">X</button>
+              <button className="drum-button">C</button>
+            </div>
           </div>
-          <div className="drum-settings">Here be settings</div>
+          <div className="drum-settings">
+            Settings
+            <div>Settings1</div>
+            <div>Settings 2</div>
+          </div>
         </div>
       </div>
     </div>
