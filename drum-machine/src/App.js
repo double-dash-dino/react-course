1b4d4630aba9bbacca94bc594513b5a5b4bd1be0
double-dash-deli <lionel.de-swartedanko@mybuilder.team>
2021-11-11 08:27:19 +0000

    create app + lay out some basics

---
+++ drum-machine/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="drum-card">
+        <div className="class-header">
+          Welcome to the drum app
+          <div className="drum-pad">
+            <button>Q</button>
+            <button>W</button>
+            <button>E</button>
+            <button>A</button>
+            <button>S</button>
+            <button>D</button>
+            <button>Z</button>
+            <button>X</button>
+            <button>C</button>
+          </div>
+          <div className="drum-settings"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
