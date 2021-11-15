1f14855b42b5e07c036fe839672ea5b9946233bd
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2021-11-15 16:30:01 +0000

    styling

--- RandomSentenceGeneratorsApp/src/Components/RandomSentence.js
+++ RandomSentenceGeneratorsApp/src/Components/RandomSentence.js
@@ -1,10 +1,14 @@
 import "./RandomSentence.css";
 
+const clickHandler = () => {};
+
 const RandomSentence = (props) => {
   return (
-    <div className="random-sentence">
-      <button className="clear-button">X</button>
-      <p>{props.sentence}</p>
+    <div className="random-sentence-box">
+      <button className="clear-button" onClick={clickHandler}>
+        X
+      </button>
+      <p className="sentence">{props.sentence}</p>
     </div>
   );
 };
